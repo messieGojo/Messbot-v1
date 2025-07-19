2e4cd9bebacdf220eb82249e57444b34c7b96143
messieGojo <198474287+messieGojo@users.noreply.github.com>
2025-07-19 20:43:32 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
-const config = require('../config');
+const axios = require('axios')
+const config = require('../config')
 
 module.exports = async function handleAI(msg, sock) {
-    const text = msg.message.conversation || msg.message.extendedTextMessage?.text || '';
-    const from = msg.key.remoteJid;
-    try {
-        const res = await axios.get(`${config.IA_ENDPOINT}${encodeURIComponent(text)}&apiKey=${config.IA_KEY}`);
-        await sock.sendMessage(from, { text: res.data.reply }, { quoted: msg });
-    } catch (e) {
-        await sock.sendMessage(from, { text: 'Erreur IA' }, { quoted: msg });
-    }
-};
+  const text = msg.message.conversation || msg.message.extendedTextMessage?.text || ''
+  const from = msg.key.remoteJid
+  try {
+    const res = await axios.get(`${config.IA_ENDPOINT}${encodeURIComponent(text)}&apiKey=${config.IA_KEY}`)
+    await sock.sendMessage(from, { text: res.data.reply }, { quoted: msg })
+  } catch (e) {
+    await sock.sendMessage(from, { text: 'Erreur IA' }, { quoted: msg })
+  }
+}
