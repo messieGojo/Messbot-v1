7b92b9b339abcc9113d354473f6588249d22f1d2
messieGojo <198474287+messieGojo@users.noreply.github.com>
2025-07-19 22:19:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  OWNER: '243999637418@s.whatsapp.net',
-  IA_ENDPOINT: 'https://messie-flash-api-ia.vercel.app/chat?prompt=',
-  IA_KEY: 'messie12356osango2025jinWoo'
+  OWNER: '243844899201@s.whatsapp.net'
 }
