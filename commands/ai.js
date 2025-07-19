25f50bd89b713c6eabc289cf4c832177935f5685
messieGojo <198474287+messieGojo@users.noreply.github.com>
2025-07-19 23:13:12 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -1,14 +1,12 @@
 const axios = require('axios')
 
-module.exports = async (msg, sock) => {
-  const text = msg.message?.conversation || msg.message?.extendedTextMessage?.text
-  if (!text) return
-
+module.exports = async function handleAI(msg, sock) {
+  const text = msg.message.conversation || msg.message.extendedTextMessage?.text
+  const from = msg.key.remoteJid
   try {
     const res = await axios.get(`https://messie-flash-api-ia.vercel.app/chat?prompt=${encodeURIComponent(text)}&key=messie12356osango2025jinWoo`)
-    const reply = res.data?.response || "Aucune réponse reçue"
-    await sock.sendMessage(msg.key.remoteJid, { text: reply }, { quoted: msg })
-  } catch {
-    await sock.sendMessage(msg.key.remoteJid, { text: "❌ Erreur IA. Serveur indisponible ou mauvais paramètre." }, { quoted: msg })
+    await sock.sendMessage(from, { text: res.data.result })
+  } catch (e) {
+    await sock.sendMessage(from, { text: 'Erreur de connexion au serveur !})
   }
 }
