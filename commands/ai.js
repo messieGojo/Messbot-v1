7b718ae8bf2396fd6a56c276178630ede90f3cdf
messieGojo <198474287+messieGojo@users.noreply.github.com>
2025-07-20 00:32:05 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -4,7 +4,7 @@ module.exports = async function handleAI(msg, sock) {
   const text = msg.message.conversation || msg.message.extendedTextMessage?.text
   const from = msg.key.remoteJid
   try {
-    const res = await axios.get(`https://messie-flash-api-ia.vercel.app/chat?prompt=${encodeURIComponent(text)}&key=messie12356osango2025jinWoo`)
+    const res = await axios.get(`https://messie-flash-api-ia.vercel.app/chat?prompt=${encodeURIComponent(text)}&apiKey=messie12356osango2025jinWoo`)
     await sock.sendMessage(from, { text: res.data.result })
   } catch (e) {
     await sock.sendMessage(from, { text: 'Erreur de connexion au serveur !'})
