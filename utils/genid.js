55eebd4c4486e7426cc79dec31fb760a9b285759
messieGojo <198474287+messieGojo@users.noreply.github.com>
2025-07-22 11:57:26 +0100

    Update genid.js

--- utils/genid.js
+++ utils/genid.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = ""
-  let characters = "ABCDEFGHIJKLMNOPSRST0123456789UVWXYZ"
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
   let charactersLength = characters.length
   for (let i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
